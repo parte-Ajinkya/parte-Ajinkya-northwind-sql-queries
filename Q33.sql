b5c802b32512553f4539a9c6398524c6ec08d9a9
parte-Ajinkya <parte.ajinkya2@gmail.com>
2023-06-08 21:29:11 +0530

    33. Find the suppliers who supply the top 5 most sold products

---
+++ Q33.sql
@@ -0,0 +1,8 @@
+/* 33. Find the suppliers who supply the top 5 most sold products */
+
+select s.company_name ,p.product_name, round(sum(o_d.unit_price * o_d.quantity - (o_d.unit_price * o_d.quantity * o_d.discount)),2) As "Revenue" from suppliers s inner join products p 
+On s.supplier_id = p.supplier_id
+inner join order_details o_d 
+On p.product_id = o_d.product_id
+group by s.supplier_id
+order by revenue DEsc limit 5;
