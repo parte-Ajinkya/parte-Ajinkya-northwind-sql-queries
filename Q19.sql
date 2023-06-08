e512fac7102301e55d4d4bfc38c7bdd75828a614
parte-Ajinkya <parte.ajinkya2@gmail.com>
2023-06-08 17:01:57 +0530

    19. Find the customers who have spent more than 000 in total

---
+++ Q19.sql
@@ -0,0 +1,8 @@
+/* 19. Find the customers who have spent more than $5000 in total */
+
+select c.company_name,sum(unit_price * quantity - (unit_price * quantity * discount)) as "spent" from customers c inner join orders o 
+On c.customer_id = o.customer_id
+Inner join order_details o_d
+On o.order_id = o_d.order_id
+group by c.customer_id
+having sum(unit_price * quantity - (unit_price * quantity * discount)) > 5000;
