54de7be54f60ead4c1086539fe6acad1715bbf23
parte-Ajinkya <parte.ajinkya2@gmail.com>
2023-06-08 21:27:40 +0530

    32. Find the employees who have not processed any orders

---
+++ Q32.sql
@@ -0,0 +1,5 @@
+/* 32. Find the employees who have not processed any orders */
+
+select e.first_name,e.last_name  from employees e left join orders o 
+On e.employee_id = o.employee_id
+where e.employee_id = Null;
