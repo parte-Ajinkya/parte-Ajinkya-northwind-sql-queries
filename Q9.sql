b6d6cb4fd348931d4a1415ce444c4aa4bf273a45
parte-Ajinkya <parte.ajinkya2@gmail.com>
2023-06-08 16:13:34 +0530

    9. Find the customers who have not placed any orders

---
+++ Q9.sql
@@ -0,0 +1,6 @@
+/* 9. Find the customers who have not placed any orders */
+
+SELECT Customers.company_name
+FROM Customers
+LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
+WHERE Orders.customer_id IS NULL;
