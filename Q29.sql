4dd92fbe9541672f4d22ef14e1f843670e99e29a
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 12:01:42 +0530

    Update Q29.sql

--- Q29.sql
+++ Q29.sql
@@ -1,8 +1,10 @@
 /* 29. Find the month in the year 2016 with the highest total sales */
 
-select Month(o.order_date) AS "SalesMonth",round(sum(o_d.unit_price * o_d.quantity - (o_d.unit_price * o_d.quantity * o_d.discount)),2) as "Sales"
+select Month(o.order_date) AS "SalesMonth",round(sum(p.unit_price * o_d.quantity - (p.unit_price * o_d.quantity * o_d.discount)),2) as "Sales"
 from orders o inner join order_details o_d
 On o.order_id = o_d.order_id       
+inner join products p
+on p.product_id = o_d.product_id
 WHERE YEAR(o.order_date) = 2016
 GROUP BY MONTH(o.order_date)
 ORDER BY Sales DESC
