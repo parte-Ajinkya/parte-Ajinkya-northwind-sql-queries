95e6ef4cff3185bffa1339062a6af3592b813ba1
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 12:47:33 +0530

    1. Get the total number of orders placed by each customer

--- Q1.sql
+++ Q1.sql
@@ -3,4 +3,4 @@
 select customers.customer_id as "Customers" ,count(orders.order_id) as "Orders"
 from customers left join orders
 On customers.customer_id =orders.customer_id
-group by customers.customer_id;
+group by customers.customer_id;  
