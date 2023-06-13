1983ec68cfac4cc9615ab4162c9a673fbef43561
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 12:07:07 +0530

    Update Q33.sql

--- Q33.sql
+++ Q33.sql
@@ -1,6 +1,6 @@
 /* 33. Find the suppliers who supply the top 5 most sold products */
 
-select s.company_name ,p.product_name, round(sum(o_d.unit_price * o_d.quantity - (o_d.unit_price * o_d.quantity * o_d.discount)),2) As "Revenue" from suppliers s inner join products p 
+select s.company_name ,p.product_name, round(sum(p.unit_price * o_d.quantity - (p.unit_price * o_d.quantity * o_d.discount)),2) As "Revenue" from suppliers s inner join products p 
 On s.supplier_id = p.supplier_id
 inner join order_details o_d 
 On p.product_id = o_d.product_id
