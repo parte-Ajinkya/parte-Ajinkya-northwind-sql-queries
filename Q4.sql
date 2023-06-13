2c4b5163312011f7ab6fa53a85bd04cf30a8137f
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 11:19:04 +0530

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,6 +1,6 @@
 /* 4. Find the total sales (Quantity*Unit_Price) for each category of products */
 
-select c.category_name, sum(o.unit_price * o.quantity ) As "total Sales"
+select c.category_name, sum(p.unit_price * o.quantity ) As "total Sales"
 from products p inner join order_details o     
 On p.product_id = o.product_id
 Inner join categories c 
