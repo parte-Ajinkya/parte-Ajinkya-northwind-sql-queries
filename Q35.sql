80faf067389ea41f3f31277842c87fc2cc5fa5b4
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 12:16:42 +0530

    Update Q35.sql

--- Q35.sql
+++ Q35.sql
@@ -1,7 +1,9 @@
 /* 35. Find the total sales for each year */
 
-select Year(o.order_date) AS "Year",round(sum(o_d.unit_price * o_d.quantity - (o_d.unit_price * o_d.quantity * o_d.discount)),2) as "TotalSales"
+select Year(o.order_date) AS "Year",round(sum(p.unit_price * o_d.quantity - (p.unit_price * o_d.quantity * o_d.discount)),2) as "TotalSales"
 from orders o inner join order_details o_d
-On o.order_id = o_d.order_id       
+On o.order_id = o_d.order_id      
+inner join products p 
+On p.product_id = o_d.product_id
 GROUP BY year(o.order_date)
-ORDER BY TotalSales;
+ORDER BY TotalSales
