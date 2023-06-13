5bffbe88a6dc3f18948fc8d2a42bf6a03289357b
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 11:27:51 +0530

    Update Q15.sql

--- Q15.sql
+++ Q15.sql
@@ -1,5 +1,7 @@
 /* 15. Find the total revenue for the year 2016 */
 
-select sum(unit_price * quantity - (unit_price * quantity * discount)) AS "Revenue" from orders o inner join order_details o_d
+select round (sum(unit_price * quantity - (unit_price * quantity * discount)),2) AS "Revenue" from orders o inner join order_details o_d
 On o.order_id = o_d.order_id
+inner join products p
+On p.product_id=o_d.product_id
 where o.shipped_date between '2016-01-01' AND '2016-12-31'; 
