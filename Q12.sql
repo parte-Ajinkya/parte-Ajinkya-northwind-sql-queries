c1e331de2c116aba0d1e791fe5ff3aead759fa67
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 11:36:58 +0530

    Update Q12.sql

--- Q12.sql
+++ Q12.sql
@@ -4,4 +4,6 @@ select e.first_name , e.last_name ,sum(unit_price * quantity - (unit_price * qua
 On e.employee_id = o.employee_id
 Inner join order_details o_d
 On o.order_id = o_d.order_id
+inner join products p 
+On p.product_id= o_d.product_id
 group by e.employee_id;
