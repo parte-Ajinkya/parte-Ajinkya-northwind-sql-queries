81d24904fbc7bef43fb2a55dbc4b190352f9985c
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2023-06-13 11:40:06 +0530

    Update Q19.sql

--- Q19.sql
+++ Q19.sql
@@ -4,5 +4,7 @@ select c.company_name,sum(unit_price * quantity - (unit_price * quantity * disco
 On c.customer_id = o.customer_id
 Inner join order_details o_d
 On o.order_id = o_d.order_id
+inner join products p 
+On p.product_id = o_d.product_id
 group by c.customer_id
 having sum(unit_price * quantity - (unit_price * quantity * discount)) > 5000;
